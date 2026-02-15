af4d970c7409ed02600bf2f28efc782760f52fa3
Florian KOLBL <florian.kolbl@gmail.com>
2026-02-16 00:24:31 +0100

    begining of burst detection. Still need improvements. FK

--- metrics/__init__.py
+++ metrics/__init__.py
@@ -7,14 +7,20 @@ from .scalar import (
 )
 from .avalanches import extract_avalanches, AvalancheResult
 from .rates import firing_rate_curve, FiringRateCurveResult
+from .isi import isi, isih, ISIResult
 from .powerlaw import fit_avalanche_powerlaw, PowerLawFitResult
 from .synchrony import PearsonSynchronyResult, pearson_corr_firing_rate
+from .burst import Burst, BurstChannelResult, BurstDetectionResult, detect_bursts
+#from .utils import _select_channels, _infer_window, pooled_spike_times
 
 __all__ = [
     "spike_count",
     "firing_rate",
     "mean_firing_rate_across_channels",
     "mean_inter_event_interval",
+    "isi",
+    "isih",
+    "ISIResult"
     "percent_random_spiking",
     "AvalancheResult",
     "extract_avalanches",
@@ -24,4 +30,8 @@ __all__ = [
     "PowerLawFitResult",
     "PearsonSynchronyResult",
     "pearson_corr_firing_rate",
+    "Burst",
+    "BurstChannelResult",
+    "BurstDetectionResult",
+    "detect_bursts",
 ]
--- plotting/__init__.py
+++ plotting/__init__.py
@@ -1,6 +1,11 @@
 from .rates import plot_firing_rate_stack, plot_firing_rate_heatmap
+from .burst import plot_bursts_spans, plot_bursts_on_raster
+from .isi import plot_logisih_threshold
 
 __all__ = [
     "plot_firing_rate_stack",
-    "plot_firing_rate_heatmap"
+    "plot_firing_rate_heatmap",
+    "plot_bursts_spans",
+    "plot_bursts_on_raster",
+    "plot_logisih_threshold",
     ]
