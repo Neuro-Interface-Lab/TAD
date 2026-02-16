acad5c0c0f2be1a2bb6f4203593f6bfe60ce010c
Florian KOLBL <florian.kolbl@gmail.com>
2026-02-16 15:49:46 +0100

    added a class to load to create triggers. FK

--- __init__.py
+++ __init__.py
@@ -1,4 +1,11 @@
 from .MCSData import MCSData
 from .raster import Raster
+from .Triggers import TimeSlot, Triggers, load_triggers_from_json
 
-__all__ = ["MCSData", "Raster"]  
+__all__ = [
+    "MCSData",
+    "Raster",
+    "TimeSlot",
+    "Triggers",
+    "load_triggers_from_json",
+    ]  
